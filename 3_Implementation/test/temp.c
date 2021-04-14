23b13448d5ee7cbaefddfaa5d703bccde09a0e63
Aditya Bakshi <81141123+AdityaBakshi5@users.noreply.github.com>
2021-04-14 15:53:37 +0530

    Update temp.c

--- 3_Implementation/test/temp.c
+++ 3_Implementation/test/temp.c
@@ -1,1 +1,35 @@
+// #include"../inc/bmi_bmr_fatPercent.h"
+// #include "../unity/unity.h"
+// #include "../unity/unity_internals.h"
+#include<stdio.h>
+#include "bmi_bmr_fatPercent.h";
+#include "unity.h"
+// #include "unity_internals.h"
 
+
+/* Required by the unity test framework */
+void setUp()
+{
+}
+/* Required by the unity test framework */
+void tearDown()
+{
+}
+       
+void test_bmr(void){
+
+    float weight = 100;
+    float height = 5;
+    
+    // TEST_ASSERT_EQUAL_FLOAT(1710.15,bmr(65,1.8,22,(char)('M'));
+    // TEST_ASSERT_EQUAL(1710.15,bmr(65,1.8,22,'M') );
+    // TEST_ASSERT_EQUAL(bmr(65,1.8,22,'M'), SUCCESS);
+    
+    TEST_ASSERT_EQUAL( 4.0000, bmi(&weight, &height));
+}
+int main(void)
+{
+UNITY_BEGIN();
+RUN_TEST(test_bmr);   
+return UNITY_END();
+}
